570a7cd3aa8684bccd919958de7b17fbd69a9784
Luís <lh.p@hotmail.com>
2023-10-01 18:33:24 -0300

    hotfix/image size

--- src/components/CardSquadPlayers/styles.ts
+++ src/components/CardSquadPlayers/styles.ts
@@ -37,8 +37,8 @@ export const ImageContent = styled.View`
 `;
 
 export const ImageProfileAndAvatar = styled.Image`
-  width: ${RFValue(62)}px;
-  height: ${RFValue(62)}px;
+  width: ${RFValue(68)}px;
+  height: ${RFValue(68)}px;
   border-radius: 5px;
 `;
 
